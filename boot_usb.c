5400f6342269b7c44d110d28350f05365285e886
Daniel Willmann <daniel@totalueberwachung.de>
2007-12-12 23:49:55 +0000

    Add "support" for RAZR2. Sending stuff works, but that's about it.

--- boot_usb.c
+++ boot_usb.c
@@ -98,6 +98,7 @@ struct phonetype phonetypes[] = {
 { "A780/E680",        0x6003, 0x02, 0x81, 0xa0200000, 0xa0400000, 0xa0000100 },
 { "A780/E680 Blob2",  0x6021, 0x02, 0x81, 0xa0300000, 0xa0500000, 0xa0000100 },
 { "E2/A1200/E6/A910", 0x6023, 0x01, 0x82, 0xa0de0000, /*FIXME*/0, 0xa0f60000 }, /* this params_addr doesnt work */
+{ "RAZR2 V8",         0x6403, 0x01, 0x82, 0xa0de0000, /*FIXME*/0, 0 }, /* XXX Address is just copied from E2/A1200 and not known to work */
 
 { "Unknown",          0x0000, 0x00, 0x00, 0x00000000, 0x00000000, 0x00000000 }
 };
