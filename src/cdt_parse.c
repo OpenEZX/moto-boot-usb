22892b9c8d028105a153eacacfb97c6a936a838a
Antonio Ospite <ospite@studenti.unina.it>
2012-01-16 17:48:27 +0100

    src/cdt_parse.c: Rewrite the parsing routines to make them more readable
    Also do the parsing in an endian independent way, and split parsing the
    file header from the partitions map.
    
    Signed-off-by: Antonio Ospite <ospite@studenti.unina.it>

--- src/cdt_parse.c
+++ src/cdt_parse.c
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,16 +24,21 @@
  * ./cdt_parse cdt.bin
  */
 
-/* TODO: make the print fuinction endiannes-safe */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stddef.h>
 #include <string.h>
 
+#include <endian.h>
+
+struct cdt_header {
+	uint16_t nparts;
+	uint16_t unknown0;
+	uint32_t unknown1;
+};
+
 struct part_info {
-	uint64_t nparts; /* used only for the first entry to tell
-			    how many entries there are in the file. */
 	uint8_t CGname[32];
 	uint16_t CGn;
 	uint16_t sig_type;
@@ -43,45 +48,70 @@ struct part_info {
 			       see http://and-developers.com/partitions:cdt */
 	uint32_t sig_start;
 	uint32_t sig_end;
-
+	uint8_t unknown[8];
 };
 
-static int parse_entry(unsigned char *buffer, struct part_info *partition)
+static inline void get_string(uint8_t **srcp, uint8_t *dest, unsigned int len)
 {
-	unsigned char *iter;
+	memcpy(dest, *srcp, len);
+	dest[len-1] = '\0';
+	*srcp += len;
+}
 
+static inline uint16_t get_le16(uint8_t **bufferp)
+{
+	uint16_t tmp;
+
+	memcpy(&tmp, *bufferp, sizeof (tmp));
+	*bufferp += sizeof (tmp);
+
+	return le16toh(tmp);
+}
+
+static inline uint32_t get_le32(uint8_t **bufferp)
+{
+	uint32_t tmp;
+
+	memcpy(&tmp, *bufferp, sizeof (tmp));
+	*bufferp += sizeof (tmp);
+
+	return le32toh(tmp);
+}
+
+static inline uint64_t get_le64(uint8_t **bufferp)
+{
+	uint64_t tmp;
+
+	memcpy(&tmp, *bufferp, sizeof (tmp));
+	*bufferp += sizeof (tmp);
+
+	return le64toh(tmp);
+}
+
+static int parse_header(uint8_t **bufferp, struct cdt_header *header)
+{
+	header->nparts = get_le16(bufferp);
+	header->unknown0 = get_le16(bufferp);
+	header->unknown1 = get_le32(bufferp);
+
+	return 0;
+}
+
+static int parse_entry(uint8_t **bufferp, struct part_info *partition)
+{
 	/* if the name is null, the we assume the entry is not valid */
-	if (buffer[sizeof (partition->nparts)] == 0)
+	if (*bufferp[offsetof(struct part_info, CGname)] == 0)
 		return 1;
 
-	iter = buffer;
-
-	memcpy(&partition->nparts, iter, sizeof (partition->nparts));
-	iter += sizeof (partition->nparts);
-
-	memcpy(&partition->CGname, iter, sizeof (partition->CGname));
-	iter += sizeof (partition->CGname);
-
-	memcpy(&partition->CGn, iter, sizeof (partition->CGn));
-	iter += sizeof (partition->CGn);
-
-	memcpy(&partition->sig_type, iter, sizeof (partition->sig_type));
-	iter += sizeof (partition->sig_type);
-
-	memcpy(&partition->start_addr, iter, sizeof (partition->start_addr));
-	iter += sizeof (partition->start_addr);
-
-	memcpy(&partition->end_addr, iter, sizeof (partition->end_addr));
-	iter += sizeof (partition->end_addr);
-
-	memcpy(&partition->base_addr, iter, sizeof (partition->base_addr));
-	iter += sizeof (partition->base_addr);
-
-	memcpy(&partition->sig_start, iter, sizeof (partition->sig_start));
-	iter += sizeof (partition->sig_start);
-
-	memcpy(&partition->sig_end, iter, sizeof (partition->sig_end));
-	iter += sizeof (partition->sig_end);
+	get_string(bufferp, partition->CGname, 32);
+	partition->CGn = get_le16(bufferp);
+	partition->sig_type = get_le16(bufferp);
+	partition->start_addr = get_le32(bufferp);
+	partition->end_addr = get_le32(bufferp);
+	partition->base_addr = get_le32(bufferp);
+	partition->sig_start = get_le32(bufferp);
+	partition->sig_end = get_le32(bufferp);
+	get_string(bufferp, partition->unknown, 8);
 
 	return 0;
 }
@@ -105,8 +135,12 @@ static void print_entry(struct part_info *partition)
 			partition->base_addr,
 			partition->sig_start,
 			partition->sig_end,
-			
+
 			(partition->end_addr - partition->start_addr + 1));
+
+#if 0
+	printf("  UNKNOWN: 0x%016lx\n", partition->unknown);
+#endif
 
 }
 
@@ -125,7 +159,7 @@ static void print_flashmap(struct part_info *flashmap, unsigned int nparts)
 	printf("|");
 	print_repeat('-', 119);
 	printf("|\n");
-	printf("| %32s | %6s | %8s | %10s | %10s | %10s | %10s | %10s | %s\n",
+	printf("| %-32s | %6s | %8s | %10s | %10s | %10s | %10s | %10s | %s\n",
 			"NAME", "CG_NUM", "SIG_TYPE", "START_ADDR", "END_ADDR",
 			"BASE_ADDR", "SIG_START", "SIG_END", "SIZE");
 
@@ -146,8 +180,9 @@ int main(int argc, char *argv[])
 {
 	FILE *fp;
 	unsigned char buffer[72];
+	unsigned char *buffer_iterator;
+	struct cdt_header header;
 	struct part_info *flashmap;
-	uint64_t nparts;
 	unsigned int i;
 	int ret = 1;
 	int err = 0;
@@ -163,7 +198,7 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	ret = fread(&nparts, 8, 1, fp);
+	ret = fread(buffer, 8, 1, fp);
 	if (ret < 1) {
 		if (feof(fp)) {
 			perror("fread nparts, feof");
@@ -174,15 +209,17 @@ int main(int argc, char *argv[])
 		}
 		exit(1);
 	}
-	rewind(fp);
 
-	flashmap = calloc(nparts, sizeof(*flashmap));
+	buffer_iterator = buffer;
+	parse_header(&buffer_iterator, &header);
+
+	flashmap = calloc(header.nparts, sizeof(*flashmap));
 	if (flashmap == NULL) {
 		perror("calloc flashmap");
 		exit(1);
 	}
 
-	for (i = 0; i < nparts; i++) {
+	for (i = 0; i < header.nparts; i++) {
 
 		ret = fread(buffer, 64, 1, fp);
 		if (ret < 1) {
@@ -196,7 +233,8 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 
-		err = parse_entry(buffer, &flashmap[i]);
+		buffer_iterator = buffer;
+		err = parse_entry(&buffer_iterator, &flashmap[i]);
 		if (err) {
 			fprintf(stderr, "Cannot parse entry.\n");
 			exit(1);
@@ -204,7 +242,7 @@ int main(int argc, char *argv[])
 	}
 	fclose(fp);
 
-	print_flashmap(flashmap, nparts);
+	print_flashmap(flashmap, header.nparts);
 
 	exit(0);
 }
