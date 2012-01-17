8e4af9ee8aba08a70f1dd2e000b3e5ba8de68ac9
Antonio Ospite <ospite@studenti.unina.it>
2012-01-17 01:24:51 +0100

    src/cdt_parse.c: Use mmap to read in the binary file
    Signed-off-by: Antonio Ospite <ospite@studenti.unina.it>

--- src/cdt_parse.c
+++ src/cdt_parse.c
@@ -29,8 +29,12 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
-
 #include <endian.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 struct cdt_header {
 	uint16_t nparts;
@@ -178,9 +182,12 @@ static void usage(const char *name)
 
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	unsigned char buffer[72];
-	unsigned char *buffer_iterator;
+	int fd;
+	int exit_code = EXIT_SUCCESS;
+	struct stat st;
+	unsigned int size;
+	uint8_t *buffer;
+	uint8_t *buffer_iterator;
 	struct cdt_header header;
 	struct part_info *flashmap;
 	unsigned int i;
@@ -192,57 +199,64 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	fp = fopen(argv[1], "r");
-	if (fp == NULL) {
-		perror(argv[1]);
-		exit(1);
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0) {
+		perror("open");
+		exit_code = EXIT_FAILURE;
+		goto out;
+	}
+	if (fstat(fd, &st) < 0) {
+		perror("fstat");
+		exit_code = EXIT_FAILURE;
+		goto out_close_fd;
+	}
+	size = st.st_size;
+
+	buffer = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+	if (buffer == NULL) {
+		perror("mmap");
+		exit_code = EXIT_FAILURE;
+		goto out_close_fd;
 	}
 
-	ret = fread(buffer, 8, 1, fp);
-	if (ret < 1) {
-		if (feof(fp)) {
-			perror("fread nparts, feof");
-		} else if (ferror(fp)) {
-			perror("fread nparts, ferror");
-		} else {
-			perror("fread nparts");
-		}
-		exit(1);
-	}
-
+	/* iterate over a copy of the pointer */
 	buffer_iterator = buffer;
+
 	parse_header(&buffer_iterator, &header);
 
 	flashmap = calloc(header.nparts, sizeof(*flashmap));
 	if (flashmap == NULL) {
 		perror("calloc flashmap");
-		exit(1);
+		exit_code = EXIT_FAILURE;
+		goto out_munmap;
 	}
 
 	for (i = 0; i < header.nparts; i++) {
-
-		ret = fread(buffer, 64, 1, fp);
-		if (ret < 1) {
-			if (feof(fp)) {
-				perror("fread, feof");
-			} else if (ferror(fp)) {
-				perror("fread, ferror");
-			} else {
-				perror("fread");
-			}
-			exit(1);
-		}
-
-		buffer_iterator = buffer;
 		err = parse_entry(&buffer_iterator, &flashmap[i]);
 		if (err) {
 			fprintf(stderr, "Cannot parse entry.\n");
-			exit(1);
+			exit_code = EXIT_FAILURE;
+			goto cleanup;
 		}
 	}
-	fclose(fp);
 
 	print_flashmap(flashmap, header.nparts);
 
-	exit(0);
+	exit_code = EXIT_SUCCESS;
+
+cleanup:
+	free(flashmap);
+
+out_munmap:
+	ret = munmap(buffer, size);
+	if (ret < 0)
+		perror("munmap");
+
+out_close_fd:
+	ret = close(fd);
+	if (ret < 0)
+		perror("close");
+
+out:
+	exit(exit_code);
 }
